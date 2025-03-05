9880ecaf9f9eb6ff4c444464fa6ea84fb498b03a
emad <emadi68moh@gmail.com>
2025-03-05 20:18:57 +0330

    remove garbage

--- startup/init.lua
+++
@@ -1,7 +0,0 @@
-local awful = require("awful")
-
-awful.spawn.with_shell("compton")
-awful.spawn.with_shell("nitrogen --restore")
-awful.spawn.with_shell("setxkbmap -model pc104 -layout us,ir -variant qwerty -option grp:ralt_rshift_toggle")
-awful.spawn.with_shell("xrandr --output HDMI-1-0 --right-of eDP-1 --auto")
--- awful.spawn.with_shell("xrandr --output DP-1 --right-of eDP-1 --auto")
--- themes/mine/promptbox.lua
+++
@@ -1,5 +0,0 @@
-local awful = require("awful")
-
-local My_prompt_box = awful.widget.prompt({ prompt = " Run : ", bg = nil })
-
-return My_prompt_box
